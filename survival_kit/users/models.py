0b7672fb760c1e83bbb74155798bbc281c027983
VadimScerbatiuc <vadim.scerbatiuc@mail.ru>
2024-02-19 23:40:54 +0200

    feat: SK-2 - Overwrited base user class, made first migration

---
+++ survival_kit/users/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Users must give an email address")
+
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserAccount(PermissionsMixin, AbstractBaseUser):
+    email = models.EmailField(verbose_name="Email", max_length=128, unique=True)
+    first_name = models.CharField(verbose_name="First name", max_length=40, blank=True, null=True)
+    last_name = models.CharField(verbose_name="Last name", max_length=40, blank=True, null=True)
+    is_staff = models.BooleanField(default=False,)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
