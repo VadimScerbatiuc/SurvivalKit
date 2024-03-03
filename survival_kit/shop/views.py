3b1d92badbf36e2c8860a9e4207acfa4094a00ca
VadimScerbatiuc <vadim.scerbatiuc@mail.ru>
2024-03-03 20:35:34 +0200

    feat: SK-5 - Added product model and page

--- survival_kit/shop/views.py
+++ survival_kit/shop/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
+
+from shop.models import Product, ProductPrice
 
 
 class ShopBasePageView(View):
@@ -7,3 +10,17 @@ class ShopBasePageView(View):
 
     def get(self, request):
         return render(request, self.template_name)
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'shop/product_list.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_price'] = 'aa'
+
+        return context
+
+
--- survival_kit/survival_kit/urls.py
+++ survival_kit/survival_kit/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from survival_kit import settings
 
 from shop.views import *
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('', include(('shop.urls', 'shop'), namespace='shop')),
     path('users/', include(('users.urls', 'users') , namespace='users')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
