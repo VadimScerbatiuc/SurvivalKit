95d394c3c0508607ad8d89b66320321a29cc1357
VadimScerbatiuc <vadim.scerbatiuc@mail.ru>
2024-03-13 21:16:54 +0200

    feat: SK-07 - Created filter using js

--- survival_kit/shop/views.py
+++ survival_kit/shop/views.py
@@ -1,23 +1,67 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
 
-from shop.models import Product, ProductPrice
+from shop.models import Product, Category, Brand
 
 
 class ShopBasePageView(View):
     template_name = 'base.html'
 
     def get(self, request):
+
         return render(request, self.template_name)
 
 
-class ProductListView(ListView):
+class ProductPageView(View):
     model = Product
-    template_name = 'shop/product_list.html'
+    template_name = 'shop/product_page.html'
     context_object_name = 'products'
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get_queryset(self, request):
+        queryset = Product.objects.all()
+        if request.GET.getlist('category'):
+            queryset = queryset.filter(category__slug__in=request.GET.getlist('category'))
+        if request.GET.get('brand'):
+            queryset = queryset.filter(brand__slug__in=request.GET.getlist('brand'))
 
-        return context
+        return queryset
+
+    def get(self, request):
+        categories = Category.objects.all()
+        brands = Brand.objects.all()
+        active_categories = request.GET.getlist('category')
+        active_brands = request.GET.getlist('brand')
+        return render(
+            request,
+            self.template_name,
+            {
+                self.context_object_name: self.get_queryset(request),
+                'categories': categories,
+                'brands': brands,
+                'active_categories': active_categories,
+                'active_brands': active_brands,
+            }
+        )
+
+
+class ProductListView(View):
+    model = Product
+    template_name = 'shop/includes/product_list.html'
+    context_object_name = 'products'
+
+    def get_queryset(self, request):
+        queryset = Product.objects.all()
+        if request.GET.getlist('category'):
+            queryset = queryset.filter(category__slug__in=request.GET.getlist('category'))
+        if request.GET.get('brand'):
+            queryset = queryset.filter(brand__slug__in=request.GET.getlist('brand'))
+
+        return queryset
+
+    def get(self, request):
+
+        return render(
+            request,
+            self.template_name,
+            {self.context_object_name: self.get_queryset(request)}
+        )
