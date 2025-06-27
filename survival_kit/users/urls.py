84b86af9dc1e5ae6e53f4a2b122eaa32232d4d19
VadimScerbatiuc <vadim.scerbatiuc@mail.ru>
2025-06-28 00:10:35 +0300

    feat:SK-16 - Added user management page

--- survival_kit/users/urls.py
+++ survival_kit/users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('sign_up/', SignUpView.as_view(), name='sign_up'),
     path('sign_in/', SignInView.as_view(), name='sign_in'),
     path('logout/', SignOutView.as_view(), name='sign_out'),
+    path('management/', ManagementPageView.as_view(), name='management'),
 ]
