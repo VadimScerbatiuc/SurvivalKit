936a6a363c093907e44b5254712751c5ab3d8889
VadimScerbatiuc <vadim.scerbatiuc@mail.ru>
2025-06-29 00:17:06 +0300

    feat:SK-18 - Changed the styl of the product creation form

--- survival_kit/shop/forms.py
+++ survival_kit/shop/forms.py
@@ -5,8 +5,8 @@ from .models import *
 class ProductCreateForm(forms.ModelForm):
     name = forms.CharField(label='Name')
     description = forms.CharField(label='Desription')
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Category')
-    brand = forms.ModelChoiceField(queryset=Brand.objects.all(), label='Brand')
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Category', empty_label="Choose category")
+    brand = forms.ModelChoiceField(queryset=Brand.objects.all(), label='Brand', empty_label="Choose brand")
     price = forms.DecimalField(label='Price')
 
     class Meta:
