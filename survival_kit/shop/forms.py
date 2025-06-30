896c0b40e5c8c1a8583b108195a2fb7ae9d38a7b
VadimScerbatiuc <vadim.scerbatiuc@mail.ru>
2025-06-30 19:20:18 +0300

    feat:SK-19 - Added small improvements

--- survival_kit/shop/forms.py
+++ survival_kit/shop/forms.py
@@ -8,7 +8,20 @@ class ProductCreateForm(forms.ModelForm):
     category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Category', empty_label="Choose category")
     brand = forms.ModelChoiceField(queryset=Brand.objects.all(), label='Brand', empty_label="Choose brand")
     price = forms.DecimalField(label='Price')
+    stock = forms.IntegerField(label='Stock')
+    is_active = forms.BooleanField(label='Is Active')
+    image = forms.ImageField(label='Main Photo')
 
     class Meta:
         model = Product
-        fields = ['name', 'description', 'category', 'brand', 'price']
+        fields = ['name', 'description', 'category', 'brand', 'price', 'stock', 'is_active']
+
+    def save(self, commit=True):
+        product = super().save(commit=commit)
+
+        image_file = self.cleaned_data.get('image')
+        print(image_file)
+        if image_file:
+            ProductImage.objects.create(product=product, image=image_file)
+
+        return product
